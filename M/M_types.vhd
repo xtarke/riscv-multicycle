117820fd6ec28d017cc8036e5864f78aee800b08
Lucas Seara Manoel <ls.manoel@gmail.com>
2019-06-29 22:57:39 -0300

    simulation running - not check

--- M/M_types.vhd
+++ M/M_types.vhd
@@ -8,17 +8,17 @@ package M_types is
 	type M_data_t is record
 		a : signed(31 downto 0);	--! Source operand A
 		b : signed(31 downto 0);	--! Source operand B
-		code  : std_logic_vector(3 downto 0);	--! Alu operation code
+		code  : std_logic_vector(2 downto 0);	--! Alu operation code
 	end record M_data_t;
 	
-	constant M_MUL: std_logic_vector(3 downto 0) := "0000";
-	constant M_MULH: std_logic_vector(3 downto 0) := "0001";
-	constant M_MULHU: std_logic_vector(3 downto 0) := "0010";
-	constant M_MULHSU: std_logic_vector(3 downto 0) := "0011";		
-	constant M_DIV: std_logic_vector(3 downto 0) := "0100";
-	constant M_DIVU: std_logic_vector(3 downto 0) := "0101";
-	constant M_REM: std_logic_vector(3 downto 0) := "0110";
-	constant M_REMU: std_logic_vector(3 downto 0) := "0111";
+	constant M_MUL: std_logic_vector(2 downto 0) :=	 	"000";
+	constant M_MULH: std_logic_vector(2 downto 0) := 	"001";
+	constant M_MULHU: std_logic_vector(2 downto 0) := 	"010";
+	constant M_MULHSU: std_logic_vector(2 downto 0) := 	"011";		
+	constant M_DIV: std_logic_vector(2 downto 0) :=	  	"100";
+	constant M_DIVU: std_logic_vector(2 downto 0) := 	"101";
+	constant M_REM: std_logic_vector(2 downto 0) :=	  	"110";
+	constant M_REMU: std_logic_vector(2 downto 0) := 	"111";
 
 end package;
 
