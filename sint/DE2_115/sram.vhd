0abf1ee0bcab40f041ca3bfb6f00d105a4a9c51d
Maria Negri <maria.rnegri@gmail.com>
2019-11-18 11:29:11 -0300

    Mudança para DE2-115

---
+++ sint/DE2_115/sram.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sram is
+	port(
+		clk : in std_logic;
+		rst : in std_logic
+	);
+end entity sram;
+
+architecture RTL of sram is
+	
+begin
+
+end architecture RTL;
