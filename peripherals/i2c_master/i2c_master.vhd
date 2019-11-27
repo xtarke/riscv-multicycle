6bf48c6fdf5a3864a2dad7a4937722ce8af9ef43
jhoulang <jhonatanlang@mail.com>
2019-11-26 21:34:38 -0300

    v0.1 - Fase pre debug

--- peripherals/i2c_master/i2c_master.vhd
+++ peripherals/i2c_master/i2c_master.vhd
@@ -10,60 +10,59 @@ entity i2c_master is
 		clk_scl : in    std_logic;      -- defasado em 90º -- PLL
 		rst     : in    std_logic;
 		ena     : in    std_logic;
-		rw      : in    std_logic;
+		rw      : in    std_logic;      -- 0 to write 1 to read
 		addr    : in    std_logic_vector(6 downto 0);
-		data_w  : in    std_logic_vector(7 downto 0)--;
---		data_r  : out   std_logic_vector(7 downto 0);
---		ack_err : out   std_logic;
---		busy    : out   std_logic
+		data_w  : in    std_logic_vector(7 downto 0);
+		--		data_r  : out   std_logic_vector(7 downto 0);
+		ack_err : out   std_logic       --;
+		--		busy    : out   std_logic
 	);
 end entity i2c_master;
 
 architecture RTL of i2c_master is
-	type state_type is (ready, start, env_addr_cm, slv_ack_cm, env_data, slv_ack_w, stop); -- faltam ainda os estados para leitura
-	signal state   : state_type := ready;
-	signal data_tx : std_logic_vector(7 downto 0);
---	signal data_rx : std_logic_vector(7 downto 0);
-	signal cnt_sda : integer    := 7;
-	signal scl_ena : std_logic;
+	type state_type is (ready, start, env_addr_cm, slv_ack_cm, rw_st, slv_ack_2, stop); -- faltam ainda os estados para leitura
+	signal state     : state_type := ready;
+	signal state_ant : state_type := ready;
+	signal data_tx   : std_logic_vector(7 downto 0);
+	signal cnt_sda   : integer    := 7;
+	signal cnt_stop  : integer;
+	signal scl_ena   : std_logic;
+	signal rw_temp   : std_logic;
+	--	signal data_rx : std_logic_vector(7 downto 0);
 begin
 
 	states_change : process(clk, rst) is
 	begin
 		if rst = '1' then
-			state 	<= ready;
-			
---busy      <= '1';           --indicate not available
---sda_int   <= '1';           --sets sda high impedance
---ack_error <= '0';           --clear acknowledge error flag
---data_rd   <= "00000000";    --clear data read port
+			state <= ready;
+		--busy      <= '1';           --indicate not available
+		--data_rd   <= "00000000";    --clear data read port
 
 		elsif rising_edge(clk) then
 			case state is
-				when ready =>			
-					scl_ena <= '0';   	--sets scl high impedance
-					sda		<= 'Z'; 	--sets sda high impedance
-			
+				when ready =>
+					scl_ena <= '0';     --sets scl high impedance
+					sda     <= 'Z';     --sets sda high impedance
+					ack_err <= '0';     --clear acknowledge error flag
+
 					if ena = '1' then
 						state <= start;
-  --busy <= '1';                   --flag busy
+						--busy <= '1';                   --flag busy
 					end if;
 
 				when start =>
 
 					cnt_sda <= 7;
-					data_tx <= addr & rw;	-- 7 bits addr device + rw bit
+					data_tx <= addr & rw; -- 7 bits addr device + rw bit
 
---start bit
-					sda     <= '0';
-				--	scl_ena <= '1';
-			
-					state   <= env_addr_cm;
+					--start bit
+					sda   <= '0';
+					state <= env_addr_cm;
 
 				when env_addr_cm =>
--- verificar se funciona
--- parte do start bit, scl sendo habilitado um ciclo atrasado
--- se não funcionar, mudar para ativalo na borda de descida do ciclo anterior
+					-- verificar se funciona
+					-- parte do start bit, scl sendo habilitado um ciclo atrasado
+					-- se não funcionar, mudar para ativalo na borda de descida do ciclo anterior
 					scl_ena <= '1';
 					if cnt_sda >= 0 then -- enquanto variavel maior que zero vai enviando o endereco
 						if data_tx(cnt_sda) = '0' then
@@ -72,81 +71,110 @@ begin
 							sda <= 'Z';
 						end if;
 
-
-	if cnt_sda = 0 then
-		sda <= 'Z';
-		state <= slv_ack_cm;
-	end if;
-	
-						cnt_sda <= cnt_sda - 1;
-
-						
-					end if;
-
-				when slv_ack_cm =>
-					
-					if sda = '0' then --- se ack ok chama proximo estado
-							cnt_sda <= 7;
-							
-							state   <= env_data;
-						else
-							state <= ready; -- se der erro volta pro inicio
-						end if;
-					
-				when env_data =>
-					scl_ena <= '1';
-					if cnt_sda >= 0 then -- enquanto variavel maior que zero vai enviando o registrador
-						if data_tx(cnt_sda) = '0' then
-							sda <= '0';
-						else
-							sda <= 'Z';
-						end if;
-
-						cnt_sda <= cnt_sda - 1;
-
 						if cnt_sda = 0 then
-							sda     <= 'Z';
-							state <= slv_ack_w;
+							state <= slv_ack_cm;
 						end if;
 
+						cnt_sda <= cnt_sda - 1;
+
 					end if;
+				when slv_ack_cm =>
+					-- intervalo onde o mestre libera sda e passa à ouvir
+					-- sda é liberado na borda de subida do clock do mestre	
+					sda <= 'Z';
+					-- na próxima borda de subida do clock do slave
 
-				when slv_ack_w =>
-					
-					if sda = '0' then --- se ack ok chama proximo estado
+					cnt_sda <= 7;
+					rw_temp <= rw;
+					data_tx <= data_w;
 
-						-- bit de parada
-						scl_ena <= '0';
+					state_ant <= state;
+					state     <= rw_st;
+
+				when rw_st =>
+
+					-- verifica o ack
+					if sda /= '0' and state_ant = slv_ack_cm then --- se não recebe ack retorna p/ ready 
+						ack_err <= '1';
 						state   <= ready;
-						
 					else
-						state <= ready; -- se der erro volta pro inicio
-						
-						
-						
-						
+						state_ant <= state;
 					end if;
+
+					-- escreve o dado
+					if rw_temp = '0' then
+						if cnt_sda >= 0 then -- enquanto variavel maior que zero vai enviando o registrador
+							if data_tx(cnt_sda) = '0' then
+								sda <= '0';
+							else
+								sda <= 'Z';
+							end if;
+
+							if cnt_sda = 0 then
+								state <= slv_ack_2;
+							end if;
+
+							cnt_sda <= cnt_sda - 1;
+						end if;
+
+--					elsif rw_temp = '1' then
+					end if;
+
+
+				when slv_ack_2 =>
+
+					-- intervalo onde o mestre libera sda e passa à ouvir
+					-- sda é liberado na borda de subida do clock do mestre	
+					sda <= 'Z';
+					-- na próxima borda de subida do clock do slave
+
+					state_ant <= state;
 					
+					if ena = '0' then
+						cnt_stop <= 1;
+						state     <= stop;
+			--		elsif rw /= rw_temp then
+			--			state     <= start;  -- ainda não preparado para receber um rw diferente, por ca
+											 -- por causa da implementação do ack
+					else					
+						cnt_sda <= 7;
+						rw_temp <= rw;
+						data_tx <= data_w;
+						
+						state     <= rw_st;
+					end if; 
+	
+				-- sda pra baixo e desabilita o scl antes de entrar no stop
+			when stop =>
+				
+				if sda /= '0' and state_ant = slv_ack_2 then --- se não recebe ack retorna p/ ready 
+						ack_err <= '1';
+						state   <= ready;
+				else
 					
-				when stop =>
+					state   <= stop;
+					
+					-- bit de parada part 1/2
+-- enable muda quando scl está em baixo, isto pode gerar uma borda de subida fora de hr
+-- analisar se há consequencias disso
+					scl_ena <= '0'; 
+					sda <= '0'; -- sda no stop bit precisa ir de baixo para alto
+								-- aqui estamos garantindo o a posição do pino após o ack				
+				
+					-- bit de parada part 2/2
+					if cnt_stop = 0 then
+						sda <= 'Z'; -- stop bit	
+						state   <= ready;
+					end if;
+				
+					cnt_stop <= cnt_stop - 1;
+				
+				end if;
+				
 				when others =>
+					state <= ready;
 			end case;
 		end if;
-	end process;
-
-	moore : process(state) is
-	begin
-		case state is
-			when ready =>
-			when start =>
-			when env_addr_cm =>
-			when slv_ack_cm =>
-			when env_data =>
-			when slv_ack_w =>
-			when stop =>
-			when others =>
-		end case;
-
 	end process;
 
 	-- quando habilitado scl recebe o clock de uma pll
