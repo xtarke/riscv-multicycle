36dd506a0b739ccf20f79b27faca0406e7b64ef6
João Antônio Cardoso <joao.maker@gmail.com>
2019-12-03 23:15:34 -0300

    top_counter adicionado no core, outputs ligadas ao barramento

--- tests/firmware.c
+++ tests/firmware.c
@@ -16,33 +16,57 @@
 #include "hardware.h"
 #include "timer.h"
 
+int main(void){
+    while(1){
+        static uint32_t events;
 
+        // test mode 0
+        TIMER_0->timer_reset = 1;
+        TIMER_0->timer_mode = 0;
+        TIMER_0->prescaler = 1;
+        TIMER_0->top_counter = 10;
+        TIMER_0->compare_0A = 2;
+        TIMER_0->compare_0B = 3;
+        TIMER_0->compare_1A = 4;
+        TIMER_0->compare_1B = 5;
+        TIMER_0->compare_2A = 6;
+        TIMER_0->compare_2B = 7;
+        TIMER_0->timer_reset = 0;
+        for(events = 4; events;){
+            if(TIMER_0->output_2A){
+                TIMER_0->timer_reset = 1;
+                events--;
+                TIMER_0->timer_reset = 0;
+            }
+        }
 
-int main(){
+        // test mode 1
+        TIMER_0->timer_reset = 1;
+        TIMER_0->timer_mode = 1;
+        TIMER_0->prescaler = 1;
+        TIMER_0->top_counter = 10;
+        TIMER_0->timer_reset = 0;
+        events = 4;
+        while(--events) while(!TIMER_0->output_0A);
 
-    //*(uint32_t *)TIMER_0 = 3;
+        // test mode 2
+        TIMER_0->timer_reset = 1;
+        TIMER_0->timer_mode = 2;
+        TIMER_0->prescaler = 2;
+        TIMER_0->timer_reset = 0;
+        events = 4;
+        while(--events) while(!TIMER_0->output_0A);
 
-	/*                 
-	TIMER_0->config.BIT.timer_reset = 1;
-	TIMER_0->config.BIT.timer_mode = 3;
-	TIMER_0->config.BIT.prescaler = 1;
-    */
+        // test mode 3
+        TIMER_0->timer_reset = 1;
+        TIMER_0->timer_mode = 3;
+        TIMER_0->prescaler = 2;
+        TIMER_0->timer_reset = 0;
+        events = 4;
+        while(--events) while(!TIMER_0->output_0A);
 
-    TIMER_0->timer_reset = 1;
+    }   
 
-    TIMER_0->timer_mode = 1;
-    TIMER_0->prescaler = 1;
-
-    TIMER_0->compare_0A = 2;
-    TIMER_0->compare_0B = 3;
-    TIMER_0->compare_1A = 4;
-    TIMER_0->compare_1B = 5;
-    TIMER_0->compare_2A = 6;
-    TIMER_0->compare_2B = 7;
-
-    TIMER_0->timer_reset = 0;
-
-    for(;;);
 
 //	uint8_t i = 0;
 //
