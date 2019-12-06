619418c100ae484d8b9fc72c2694d4f548134a79
diogo0001 <diogo.t0001@gmail.com>
2019-12-06 13:18:43 -0300

    General Updates

---
+++ VHDL Tutorial/step_7_your_own_project.vhd
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------------
+-- If you got this far, you have learned the basics about working with Sigasi Studio.
+--
+-- Congratulations!
+--
+-- TODO "Set up your own project"
+--
+-- You are now ready to go and set up a project for your own VHDL code.
+-- Go to this webpage to lean how (you can hold CTRL and click on the link):
+-- <http://www.sigasi.com/app/project-setup>
+--------------------------------------------------------------------------------
