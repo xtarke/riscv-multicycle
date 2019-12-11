b4eddbc4cce729869314151207d69d3673aa1a92
jhoulang <jhonatanlang@mail.com>
2019-12-11 13:10:40 -0300

    fix time simulation

--- tests/firmware.c
+++ tests/firmware.c
@@ -25,30 +25,34 @@ int I2C_write(uint8_t data, uint8_t addr){
 	// ack_err				rst ena rw  addr data			--futuro "1 bit busy"   --sda e scl?
 
 	//pulso no rst
-	I2C |= 0x40000000;
+	I2C = 0x40000000;
 	delay_(20);
-	I2C &= 0xBFFF8000;  // limpa o rst e os campos addr e data
+	I2C = 0x00000000;  // limpa o rst e os campos addr e data
 
-	I2C |=  data;		// carrega dado
-	I2C |=  (addr << 8);// carrega endereço
+	I2C =  0x20000000 + (addr * 128) + data;// carrega endereço
+	I2C =  0x00000000 + (addr * 128) + data;// carrega endereço
+	//I2C *= 128;
+	//I2C |= data;		// carrega dado
 
 	// pulso no enable
-	I2C |= 0x00008000;
-	delay_(20);
-	I2C &= 0xFFFF7FFF;
-
-	delay_(300);
+//	I2C = I2C | 0x20000000;
+	delay_(2);
+	//I2C = I2C - 0x20000000;
+	//I2C &= 0xDFFFFFFF;
+	I2C = 0;
+	delay_(10);
 	//le o ack
 	ack = (I2C >> 31);
 
+	return ack;
 }
 
 
 int main(){
 
 
-//	int ack;
-	SEGMENTS = 0xFFFFFFF0;
+	int ack;
+/*	SEGMENTS = 0xFFFFFFF0;
 
 	uint8_t i = 0;
 	
@@ -71,18 +75,18 @@ int main(){
 		/* To test Data Bus
 		x = INBUS;        
 		OUTBUS = x;*/
-	}
+//	}
 
 
 
-/*	while(1)
+	while(1)
 	{
-		I2C |= 0x40000000;
-		//ack = I2C_write( 0xFF, 0b01101000);
+	//	I2C |= 0x40000000;
+		ack = I2C_write( 0xFF, 0b01101000);
 		delay_(100);
 
 	}
-*/
+
 
 
 
