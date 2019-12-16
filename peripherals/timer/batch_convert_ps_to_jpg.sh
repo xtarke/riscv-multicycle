a09d817020f50603cf245377a4a9fdac24d35f24
João Antônio Cardoso <joao.maker@gmail.com>
2019-12-15 22:04:54 -0300

    fixing imgs orientation

--- peripherals/timer/batch_convert_ps_to_jpg.sh
+++ peripherals/timer/batch_convert_ps_to_jpg.sh
@@ -4,6 +4,10 @@ PATH=${1:-.}
 
 for image in ${PATH}/*.ps; do
     echo ${image}
-    /usr/bin/gs -sDEVICE=jpeg -r300 -sPAPERSIZE=a4 -dBATCH -dNOPAUSE \
+    /usr/bin/gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=jpeg -sPAPERSIZE=a4  \
+         -r300 \
         "-sOutputFile=${image%.ps}.jpg" "$image"
+done
+for image in ${PATH}/*.jpg; do
+     /usr/bin/convert ${image} -rotate -270 ${image}
 done
