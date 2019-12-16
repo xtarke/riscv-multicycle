66735566bdbce7d6beb189f823ed2191f02d2909
João Antônio Cardoso <joao.maker@gmail.com>
2019-12-15 21:41:27 -0300

    fixing imgs orientation

--- peripherals/timer/batch_convert_ps_to_jpg.sh
+++ peripherals/timer/batch_convert_ps_to_jpg.sh
@@ -4,6 +4,8 @@ PATH=${1:-.}
 
 for image in ${PATH}/*.ps; do
     echo ${image}
-    /usr/bin/gs -sDEVICE=jpeg -r300 -sPAPERSIZE=a4 -dBATCH -dNOPAUSE \
+    /usr/bin/gs -dSAFER -dBATCH -dNOPAUSE -dAutoRotatePages=/None \
+        -c "<</Orientation 3>> setpagedevice" \
+        -sDEVICE=jpeg -sPAPERSIZE=a4 -r300 \
         "-sOutputFile=${image%.ps}.jpg" "$image"
 done
