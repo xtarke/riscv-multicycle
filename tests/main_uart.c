63a9541c045dfbee29200607fec6fe3e05c5f7aa
Daniel Pereira <20281349+Adjucaris@users.noreply.github.com>
2019-12-16 21:41:06 -0300

    Update main_uart.c

--- tests/main_uart.c
+++ tests/main_uart.c
@@ -2,7 +2,7 @@
  * firmware.c
  *
  *  Created on: Jan 20, 2019
- *      Author: Marcos Vinicius Leal Da Silva
+ *      Author: Marcos Vinicius Leal Da Silva e Daniel Pereira
  *      Instituto Federal de Santa Catarina
  * 
  * 
@@ -21,17 +21,20 @@ int main(){
 	
 	int x;
 	
+	UART_setup(3, 1);
+	
 	while (1){
+		// Testing UART - Adjusts
+		//delay_(10000);
+	
 		// Testing UART - Transmission
-		UART_write('b');
+		UART_write('c');
 		delay_(10000);
 		
 		// Testint UART - Reception
 		x = UART_read();
 		OUTBUS = x;
 		delay_(10000);
-		
-
 	}
 
 	return 0;
