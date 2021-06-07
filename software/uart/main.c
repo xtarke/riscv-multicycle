5cc0e1c4a6ac77995f29e2bd6d739d2bec6b0cc6
Renan Augusto Starke <xtarke@gmail.com>
2021-06-07 11:57:53 -0300

    Added UART main.c files

---
+++ software/uart/main.c
@@ -0,0 +1,43 @@
+/*
+ * firmware.c
+ *
+ *  Created on: Jan 20, 2019
+ *      Author: Marcos Vinicius Leal Da Silva e Daniel Pereira
+ *      Instituto Federal de Santa Catarina
+ *
+ *
+ * Simple UART TX example.
+ * -----------------------------------------
+ */
+
+
+ #include <limits.h>
+ #include "uart.h"
+ #include "../_core/utils.h"
+ #include "../_core/hardware.h"
+
+
+int main(){
+
+	int x;
+
+	UART_setup(0, 0);
+
+	UART_write('j');
+
+	while (1){
+		// Testing UART - Adjusts
+		//delay_(10000);
+
+		// Testing UART - Transmission
+		//UART_write('c');
+		delay_(10);
+
+		// Testint UART - Reception
+		//x = UART_read();
+		//OUTBUS = x;
+		//delay_(10000);
+	}
+
+	return 0;
+}
---
+++ software/uart/main_irq.c
@@ -0,0 +1,45 @@
+/*
+ * firmware.c
+ *
+ *  Created on: Jan 20, 2019
+ *      Author: Marcos Vinicius Leal Da Silva e Daniel Pereira
+ *      Instituto Federal de Santa Catarina
+ *
+ *
+ * Simple LED blink example.
+ * -----------------------------------------
+ */
+
+#include <limits.h>
+#include "uart.h"
+#include "../_core/utils.h"
+#include "../_core/hardware.h"
+#include "../gpio/gpio.h"
+#include "../irq/interrupt.h"
+
+
+void UART_IRQHandler(void){
+	uint8_t data;
+	data = UART_read();
+	OUTBUS = data;
+	HEX0 = data;
+	HEX1 = data >> 4;
+}
+
+
+int main(){
+
+	uint8_t data = 0x0F;
+	UART_setup(0, 0);
+	UART_interrupt_enable();
+
+	extern_interrupt_enable(true);
+	global_interrupt_enable(true);
+
+	while (1){
+
+		delay_(100);
+	}
+
+	return 0;
+}
