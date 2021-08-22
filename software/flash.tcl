2e7c6caff84824f78f31f91eef2bbe4250ee5178
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2021-08-22 09:26:18 -0300

    script flash.tcl - add "3.1" no regex do parametro 'hardware_name'
    Sem essa modificação o DE10-Lite no meu PC não estava sendo encontrado pelo script de flash

--- software/flash.tcl
+++ software/flash.tcl
@@ -1,4 +1,4 @@
-begin_memory_edit -hardware_name "USB-Blaster \[1-2\]" -device_name "@1: 10M50DA(.|ES)/10M50DC (0x031050DD)"
+begin_memory_edit -hardware_name "USB-Blaster \[1-3.1\]" -device_name "@1: 10M50DA(.|ES)/10M50DC (0x031050DD)"
 
 update_content_to_memory_from_file -instance_index 0 -mem_file_path "quartus_blink.hex" -mem_file_type "hex"
 
