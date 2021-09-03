7f078fe8e58e34b2beca7b0474ee449f9ddff861
pc <ednei_f@hotmail.com>
2021-09-03 10:12:18 -0300

    Filtro média móvel final

--- software/dig_filt/dig_filt.c
+++ software/dig_filt/dig_filt.c
@@ -1,24 +1,16 @@
-/* Digital filter peripheral HAL
-   Tarcis Aurélio Becher  14/10/2020
+/* Digital filter peripheral
 */
 
 #include "dig_filt.h"
 
-uint32_t dig_filt_data_is_ready(){
-	return (DIG_FILT_CTRL >> 31)&(1);
+void dig_filt_reset(uint8_t data){
+  DIG_FILT_CTRL-> reset = data;
 }
 
-void dig_filt_reset(){
-	DIG_FILT_CTRL = RESET_REGS;
-	while(!dig_filt_data_is_ready());
-}
-
-void dig_filt_add_data(uint32_t data_in){
-	DIG_FILT_IN = data_in;
-	DIG_FILT_CTRL = NEW_DATA;
-	while(!dig_filt_data_is_ready());
+void dig_filt_enable(uint8_t data){
+  DIG_FILT_CTRL-> enable = data;
 }
 
 uint32_t dig_filt_get_output(){
-	return DIG_FILT_OUT;	
+  return (DIG_FILT_OUT);
 }
