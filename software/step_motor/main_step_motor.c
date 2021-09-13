100ebb9fa42aa263d7ed9171bfe9c0c4ef30ca9d
Rayan Steinbach <rayansteinbach97@gmail.com>
2021-09-13 18:51:54 -0300

    Final integration with riscVcore

--- software/step_motor/main_step_motor.c
+++ software/step_motor/main_step_motor.c
@@ -1,38 +1,28 @@
 #include <stdint.h>
 #include "step_motor.h"
-#include "../gpio/gpio.h"
 
 int main() {
-    uint8_t spd = 0;
-    
+    reset_motor(1);
     while(1) {
-        if (INBUS & MSK_RST == 1)
-            reset_motor(1);
-        else
-            reset_motor(0);
+    reset_motor(0);
+    change_speed(5);
+    change_step(1);
+    stop_motor(0);
+    reverse_rotation(0);
+    delay_(2);
 
-        if (INBUS & MSK_STOP == 2) 
-            stop_motor(1);
-        else
-            stop_motor(0);
+    change_step(0);
+    delay_(1);
 
-        if (INBUS & MSK_REV == 4)
-            reverse_rotation(1);
-        else
-            reverse_rotation(0);
+    stop_motor(1);
+    delay_(3);
+    reverse_rotation(1);
+    delay_(1);
 
-        if (INBUS & MSK_HF == 8)
-            change_step(1);
-        else
-            change_step(0);
+    change_speed(0);
+    delay_(1);
+    
 
-        if(spd > 7){
-            spd = 0;
-        }
-
-        change_speed(7);
-        
-        delay_(100000);
     }
     return 0;
 }
--- software/step_motor/step_motor.c
+++ software/step_motor/step_motor.c
@@ -14,7 +14,7 @@ void reverse_rotation(uint8_t val) {
 }
 
 void change_step(uint8_t val) {
-    STEP_BASE->half_full = val
+    STEP_BASE->half_full = val;
 }
 
 void change_speed(uint8_t val) {
