aa6ef13091e97b05bdac4181eca3199693fc8f47
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2021-09-19 23:01:07 -0300

    flash bus software - quartus test; refactoring; tcl script
    quartus testing is returning error!

--- software/flash/main_flash.c
+++ software/flash/main_flash.c
@@ -2,25 +2,38 @@
  * Simple FLASH example.
  */
 
+#define SUCCESS 1
+#define ERROR 0
+
 #include "../_core/utils.h"
 #include "../_core/hardware.h"
 #include "../gpio/gpio.h"
 
 
-void test_write_read();
+void test_modelsim_simulation();
+int test_quartus();
 
 int main() {
 
-	test_write_read();
+	// test_modelsim_simulation();
+	// return 0;
+
+	while (1) {
+		int retval = test_quartus();
+
+		if (retval == ERROR)
+			while (1){};
+	}
+
 
 	return 0;
 }
 
-void test_write_read() {
+void test_modelsim_simulation() {
 
    	volatile uint32_t *flash = &FLASH;
 
-	// consecutive write operations don't require delay in between.
+	// consecutive write/read operations don't require delay in between.
 
 	// flash[0] = 0xAABBCCDD;
 	flash[0] = 0xAA000000;
@@ -33,4 +46,25 @@ void test_write_read() {
 	y = flash[1];
 	z = flash[2];
 	w = flash[3];
+}
+
+int test_quartus() {
+
+	volatile uint32_t *flash = &FLASH;
+	
+	for (uint32_t i = 0; i < 64; i++) {
+		flash[i] = i;
+	}
+
+	for (uint32_t i = 0; i < 64; i++) {
+		if (flash[i] == i) {
+			OUTBUS = i;
+			delay_(10000);
+		} else {
+			OUTBUS = 0xFF;
+			return ERROR;
+		}
+	}
+
+	return SUCCESS;
 }
