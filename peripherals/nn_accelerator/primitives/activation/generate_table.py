7a64c944b34dd68be554da6aac60bff11378199f
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-02-27 20:07:16 -0300

    neuron primitive

--- peripherals/nn_accelerator/primitives/activation/generate_table.py
+++ peripherals/nn_accelerator/primitives/activation/generate_table.py
@@ -9,14 +9,21 @@ def sigmoidal(val: float) -> float:
 def tanh(val: float) -> float:
     raise NotImplementedError()
 
+def heaviside(val: float) -> float:
+    if val > 0:
+        return 1
+    else:
+        return 0
+
 def scale_to_full_range(val: float, n_bits) -> int:
     return int(val * (2**n_bits//2 -1))
 
 n_bits: int = 8
-f = sigmoidal
+#f = sigmoidal
+f = heaviside
 prefix = '			'
 for i in range(-2**n_bits//2, 2**n_bits//2):
     #print(i, twos_complement(i, n_bits))
     #print(i, scale_to_full_range(f(i), n_bits))
     val = scale_to_full_range(f(i), n_bits)
-    print(f'{prefix}when "{twos_complement(i, n_bits)}" => segs <= "{twos_complement(val, n_bits)}";	-- {i}')
+    print(f'{prefix}when "{twos_complement(i, n_bits)}" => output <= "{twos_complement(val, n_bits)}";	-- {i}')
