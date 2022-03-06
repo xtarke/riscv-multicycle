d423563ceba85da05a3e878ff011f5d7c15735a0
Igor Debastiani <godinho.igor@gmail.com>
2022-03-06 18:00:13 -0300

    fix: reorganiza código C

--- software/lcd_hd44780/lcd_hd44780.c
+++ software/lcd_hd44780/lcd_hd44780.c
@@ -7,19 +7,23 @@
 
 int main(){
   unsigned int i;
-
-    HD44780_REGISTER -> start = 1;
-    HD44780_REGISTER -> wr_en = 0;
-    delay_(500);
-    HD44780_REGISTER -> start = 0;
-    HD44780_REGISTER -> clear = 1;
-    delay_(500);
-    HD44780_REGISTER -> clear = 0;
-  while(1) {    
-    // HD44780_REGISTER -> character = '01000001'; //'B'; //0b01000001;
+  HD44780_REGISTER -> start = 0;
+  HD44780_REGISTER -> wr_en = 0;
+  HD44780_REGISTER -> clear = 0;
+  HD44780_REGISTER -> goL1 = 0;
+  HD44780_REGISTER -> goL2 = 0;
+  delay_(500);
+  HD44780_REGISTER -> start = 1;
+  delay_(500);
+  HD44780_REGISTER -> start = 0;
+  delay_(500);
+  HD44780_REGISTER -> clear = 1;
+  delay_(500);
+  HD44780_REGISTER -> clear = 0;
+  while(1) {
     HD44780_REGISTER -> wr_en = 1;
+    HD44780_REGISTER -> character = (int) 'C';
     delay_(500);
-    
   }
   return 0;
 }
