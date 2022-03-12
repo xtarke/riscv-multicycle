43c53e25f715b14710bc29f3a2cf6229b6e81358
Caroles12 <contatofariascarol@gmail.com>
2022-03-12 18:53:58 -0300

    Retirando funções comentadas do filtro fir

--- software/dig_filt/fir_filt.c
+++ software/dig_filt/fir_filt.c
@@ -3,14 +3,6 @@
 
 #include "fir_filt.h"
 
-/*void fir_filt_reset(uint32_t data){
-  FIR_FILT_CTRL-> reset = data;
-}
-
-void fir_filt_enable(uint32_t data){
-  FIR_FILT_CTRL-> enable = data;
-}*/
-
 void fir_filter_coefficient0(uint32_t coefficient){
 
     FIR_FILT_CTRL-> coef0 = coefficient;
