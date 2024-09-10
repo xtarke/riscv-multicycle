8fc665ed2d6d3437b610c077aaca47c5a641211c
RamonSerafim <64420659+RamonSerafim@users.noreply.github.com>
2024-09-10 08:35:04 -0300

    Add files via upload

---
+++ peripherals/spwm/testbench_spwm.vdh.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity filename is
+    port(
+        clk : in std_logic;
+        rst : in std_logic
+    );
+end entity filename;
+
+architecture RTL of filename is
+    
+begin
+
+end architecture RTL;
