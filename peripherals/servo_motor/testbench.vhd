734d0804ded15fd907c645f5a0d532381b65fb14
Thaine Martini <thainemartini@outlook.com>
2025-02-23 22:27:33 -0300

    Update testbench.vhd
    testbench pwm

--- peripherals/servo_motor/testbench.vhd
+++ peripherals/servo_motor/testbench.vhd
@@ -1,15 +1,27 @@
+-- ============================================================================
+-- Name        : testbench.vhd
+-- Author      : Thaine Martini
+-- Version     : 0.1
+-- Description : Script de teste do pwm.vhd
+-- ============================================================================
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity testbench is
+    generic(
+        constant N : integer:=31
+    );
 end entity testbench;
+
 
 architecture rtl of testbench is
     
     signal clk_tb : std_logic; 
     signal rst_tb : std_logic;
-    signal rot_tb : integer range 0 to 100;
+    signal rot_tb : unsigned(N downto 0);
     signal pwm_tb : std_logic;  
     --signal test_tb : integer range 0 to 20000;
     
@@ -18,11 +30,13 @@ architecture rtl of testbench is
 begin
     
     pwm_inst : entity work.pwm
+    generic map(
+            P => N
+        )
         port map(
             clk  => clk_tb,
             rst  => rst_tb,
             rotate => rot_tb,
-            --test => test_tb,
             pwm => pwm_tb
             
         );   
@@ -37,9 +51,9 @@ begin
 
     process is
     begin
-        rot_tb <= 30;
+        rot_tb <= to_unsigned(30,rot_tb'length);
         wait for 20000001 ns; 
-        rot_tb <= 20;
+        rot_tb <= to_unsigned(20,rot_tb'length);
         wait for 1000000 ns;
     end process;
 
