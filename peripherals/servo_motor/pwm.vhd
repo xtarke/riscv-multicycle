abc7776488fe6cde6670e4c8890cf97331366bf6
Thaine Martini <thainemartini@outlook.com>
2025-02-23 22:24:52 -0300

    Update pwm.vhd

--- peripherals/servo_motor/pwm.vhd
+++ peripherals/servo_motor/pwm.vhd
@@ -3,7 +3,8 @@
 -- Author      : Thaine Martini
 -- Version     : 0.1
 -- Copyright   : Thaine, Departamento de Eletrônica, Florianópolis, IFSC
--- Description : 
+-- Description : Gera umm Pwm de 50 Hz com um duty cycle que varia de 
+--	         5us a 1,5ms. Utiliza o contador.vhd para isso
 -------------------------------------------------------------------
 
 library ieee;
@@ -11,22 +12,25 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
         
 entity pwm is
-     
+    generic (
+        constant P: natural :=  31
+    );
     port (
         clk : in std_logic;
         rst : in std_logic;
-        rotate: in integer range 0 to 100;
+        rotate: in unsigned(P downto 0);
         pwm : out std_logic
-       -- test : out integer range 0 to 2000
-
     );
 end entity pwm;
 
 architecture rtl of pwm is
-    signal contador : integer range 0 to 20000;
+    signal contador : unsigned (P downto 0);
 begin
 
-    countador_inst : entity work.contador
+    contador_inst : entity work.contador
+    generic map(
+            T => P
+        )
         port map(
             clk  => clk,
             rst  => rst,
@@ -35,23 +39,23 @@ begin
 
     process (clk, rst) 
 
-        variable x: integer range 0 to 2000;
+        variable x: integer range 0 to 200000;
+        --(63 downto 0);
 
     begin
             
         if rst = '1' then 
-			pwm <= '0';
+			pwm <= '1';
             x:=0;
-
+    
         elsif rising_edge(clk) then
-            x:= rotate * 15;
-            x:= x + 500;
+            x:= 100 * to_integer(rotate);   
+            x:= x + 5000;
             if contador < x then
                 pwm <= '1';
             else
                 pwm <= '0';
             end if;
         end if;
-        --test <=x;
     end process;
 end architecture rtl;
