c23855615e52c4947218cfeef7d091943b92e246
Kenner <kenner.marqueti@gmail.com>
2025-03-02 22:08:41 -0300

    updating software for accelerometer

--- software/accelerometer/main_accelerometer.c
+++ software/accelerometer/main_accelerometer.c
@@ -1,6 +1,6 @@
 /*
  *
- *    Aluno: Victor Lompa Schwider
+ *    Aluno: Victor Lompa Schwider (edit: Kenner Marqueti Couto , 18 Feb 2025)
  *    Data : 07 Dez 2023
  */
 #include "accelerometer.h"
@@ -8,28 +8,53 @@
 #include "../_core/utils.h"
 #include "../gpio/gpio.h"
 
+uint32_t complemento (uint32_t x) {
+    // Check if the number is negative (MSB is 1)
+    if (x & 0x80000000) {
+        return ~x + 1; // Compute two's complement (negate)
+    }
+    return x; // Already positive
+}
+
 int main(void){
     const int sense = 10; // Bubble effect sensibility
     uint32_t axe;
+    uint32_t entrada;
+    int32_t signed_axe;
+    uint8_t abs_axe;
+
+
+   
+       // Bubble effect with leds based on axe value
+    uint32_t bubble[19] =  {0x0200, 0x0300, 0x0100, 0x0180,
+                            0x0080, 0x00c0, 0x0040, 0x0060,
+                            0x0020, 0x0030, 0x0010, 0x0018, 
+                            0x0008, 0x000c, 0x0004, 0x0006, 
+                            0x0002, 0x0003, 0x0001};
+
     while(1)
     {
-        if      (((INBUS&0x3)) == 1){      // If SW(0) is on return axe_x values
+        entrada = (INBUS&0X7);
+        if      (((entrada)) == 1){      // If SW(0) is on return axe_x values
             axe = read_axe_x();}
-        else if (((INBUS&0x3)) == 2){      // If SW(1) is on return axe_y values
+        else if (((entrada)) == 2){      // If SW(1) is on return axe_y values
             axe = read_axe_y();}
-        else if (((INBUS&0x3)) == 3){      // If SW(0) and SW(1) is on return axe_z values
-            axe = read_axe_z();}    
+        else if (((entrada)) == 3){      // If SW(0) and SW(1) is on return axe_z values
+            axe = read_axe_z();}
+        else if (((entrada)) == 5){      // If SW(2) AND SW(0) is on return axe_x_max values
+            axe = read_axe_x_max();}
+        else if (((entrada)) == 6){      // If SW(2) and SW(1) is on return axe_y_max values
+            axe = read_axe_y_max();}  
+        else if (((entrada)) == 7){      // If SW(2) and SW(1) and SW(0) is on return axe_z_max values
+            axe = read_axe_z_max();}   
         else{                              // If none of the above, return 0
             axe = 0;} 	
+        
+        signed_axe = (int32_t) axe;
+        abs_axe = (uint8_t)((signed_axe < 0) ? -signed_axe : signed_axe);
+        //axe = (signed_axe < 0) ? -signed_axe : axe;
 
-
-        // Bubble effect with leds based on axe value
-        uint32_t bubble[19] =  {0x0200, 0x0300, 0x0100, 0x0180,
-                                0x0080, 0x00c0, 0x0040, 0x0060,
-                                0x0020, 0x0030, 0x0010, 0x0018, 
-                                0x0008, 0x000c, 0x0004, 0x0006, 
-                                0x0002, 0x0003, 0x0001};
-
+//        SEGMENTS = "0xABCD12";
 
         // Negative values
         // Must not be higher than FCFF otherwise it will pop up the led on the positive side
@@ -57,8 +82,11 @@ int main(void){
 
         else {OUTBUS = 0x0000;}
 
+        SEGMENTS = abs_axe;
         // Check if values are working on hardware with fixed value
         // OUTBUS = 0x2ef;
+
+        
     }
 	return 0;
 }
