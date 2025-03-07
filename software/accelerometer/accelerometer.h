f856c55fc2c44c9ff2109e46de7e64130786c689
Kenner Marqueti Couto <74597805+Ghost7600@users.noreply.github.com>
2025-03-07 16:17:36 -0300

    max values work, but have to treat negative data, read as big values (#82)
    * max values work, but have to treat negative data, read as big values
    
    * updating software for accelerometer
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Kenner <kenner.marqueti@gmail.com>

--- software/accelerometer/accelerometer.h
+++ software/accelerometer/accelerometer.h
@@ -11,6 +11,9 @@ typedef struct
 	_IO32 axe_x; //	0x0000, 128, x80
 	_IO32 axe_y; //	0x0001, 129, x81
 	_IO32 axe_z; //	0x0002, 130, x82
+	_IO32 axe_x_max; //	0x0000, 131, x83
+	_IO32 axe_y_max; //	0x0001, 132, x84
+	_IO32 axe_z_max; //	0x0002, 133, x85
 } ACCEL_TYPE;
 
 #define ACCEL ((ACCEL_TYPE *) &ACCELEROMETER_BASE_ADDRESS)
@@ -18,5 +21,8 @@ typedef struct
 uint32_t read_axe_x(void);
 uint32_t read_axe_y(void);
 uint32_t read_axe_z(void);
+uint32_t read_axe_x_max(void);
+uint32_t read_axe_y_max(void);
+uint32_t read_axe_z_max(void);
 
 #endif //ACCELEROMETER_H
