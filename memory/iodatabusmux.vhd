1617ddc5207a071d86af476db4c27edc6650b4f7
Fabio Schmitt Avelino <72568537+Fsavellino@users.noreply.github.com>
2025-07-22 18:48:59 -0300

    Update iodatabusmux.vhd
    Adicionado linha com endereço para memório usado no periférico do led_rgb

--- memory/iodatabusmux.vhd
+++ memory/iodatabusmux.vhd
@@ -31,7 +31,8 @@ entity iodatabusmux is
         ddata_r_key             : in   std_logic_vector(31 downto 0);
         ddata_r_accelerometer   : in  std_logic_vector(31 downto 0);
         ddata_r_cordic          : in std_logic_vector(31 downto 0);
-     	  ddata_r_RS485   		: in  std_logic_vector(31 downto 0);
+     	ddata_r_RS485   		: in  std_logic_vector(31 downto 0);
+        ddata_r_rgb             : in  std_logic_vector(31 downto 0);
         -- Mux 
         ddata_r_periph   : out std_logic_vector(31 downto 0) --! Connect to data bus mux
     );
@@ -59,7 +60,8 @@ begin
         ddata_r_spwm  when x"0011",
         ddata_r_accelerometer when x"0012",
         ddata_r_cordic when x"0015",
-    		ddata_r_RS485 when x"0017",
+    	ddata_r_RS485 when x"0017",
+        ddata_r_rgb when x"0020",
         -- Add new io peripherals here
         (others => '0') when others;
 end architecture RTL;
