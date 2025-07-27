e33cd5c06d401307c2984ee133963c4e89aa6002
Joana Wasserberg <60950826+joananana@users.noreply.github.com>
2025-07-27 06:45:51 -0300

    Update clock_divider.vhd
    adiciona comentários explicativos

--- peripherals/animation_7segs/clock_divider.vhd
+++ peripherals/animation_7segs/clock_divider.vhd
@@ -2,8 +2,9 @@
 -- Name        : divisor_clock.vhd
 -- Author      : Exemplo baseado em PEDRONI, Eletrônica Digital Moderna e VDHL
 -- Version     : 0.1
--- Copyright   : Renan, Departamento de Eletrônica, Florianópolis, IFSC
--- Description : Exemplo de divisor de clock.
+-- Copyright   : Adaptação do código do professor Renan, Departamento de Eletrônica, Florianópolis, IFSC
+-- Description : Divisor de clock com controle de velocidade.
+--               Gera um clock de saída com frequência menor, ajustável pelo sinal "speed".
 -------------------------------------------------------------------
 
 -- Bibliotecas e clásulas
@@ -13,30 +14,35 @@ use ieee.numeric_std.all;
 
 -- Entidade e portas
 ENTITY divisor_clock IS
-    PORT (clk, rst, ena: IN std_logic;
-          speed : in std_logic_vector(1 downto 0);
-          clk_output: OUT std_logic);
+   PORT(
+        clk        : IN std_logic;                     -- Clock de entrada (rápido)
+        rst        : IN std_logic;                     -- Reset síncrono
+        ena        : IN std_logic;                     -- Habilita o divisor
+        speed      : IN std_logic_vector(1 downto 0);  -- Seleção da velocidade (2 bits)
+        clk_output : OUT std_logic                     -- Clock dividido gerado
+    );
 END ENTITY;
 
--- Arquitetura
 ARCHITECTURE rtl OF divisor_clock IS
 begin
 
     p0: process(clk)
+        -- Contador usado para dividir a frequência
         variable count : natural := 0;
+        -- Sinal temporário para armazenar o clock gerado
         variable temp  : std_logic := '0';
+        -- Valor máximo do contador, ajustável via sinal de velocidade
         variable max   : natural := 10000;
 
     begin
-	 
-		
-		if rst = '1' then
+
+	if rst = '1' then
             count := 0;
             temp := '1';
 				
-      elsif (rising_edge(clk)) then
+	elsif (rising_edge(clk)) then
                 case speed is
-                    when "11" => max := 500;
+                    when "11" => max := 500; --Mais rápido
                     when "10" => max := 1000;
                     when "01" => max := 2500;
                     when "00" => max := 10000;
@@ -45,7 +51,7 @@ begin
 
             if ena = '1' then
                 count := count + 1;
-                if (count >= max) then
+                if (count >= max) then 
                     temp := not temp;
                     count := 0;
                 end if;           
