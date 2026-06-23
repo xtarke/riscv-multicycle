5df66f685b3531f21420a826805149e52a95e2ab
alejopm03 <perdomomilar@gmail.com>
2026-06-23 13:21:14 -0300

    Update VGA software: fix toolchain path and add color test pattern

--- software/vga/main_vga.c
+++ software/vga/main_vga.c
@@ -1,33 +1,77 @@
 /*
- * firmware.c
+ * main_vga.c
  *
  *  Created on: Jan 20, 2019
  *      Author: Renan Augusto Starke
  *      Instituto Federal de Santa Catarina
- * 
- * 
- * Simple LED blink example.
+ *
+ * VGA test: draws a small colored square using the 8K word VGA RAM.
+ * The VGA controller wraps addresses with 13 bits, so only addresses
+ * 0..8191 are visible. At 800 pixels per row, 8192 words cover ~10
+ * rows. We draw a 64x64 square in the top-left area.
  * -----------------------------------------
  */
-
 
 #include "../_core/utils.h"
 #include "../_core/hardware.h"
 
-#define VGA_RAM_SIZE 8192
+/* VGA RAM is mapped at SDRAM address space (dcsel = "11") */
+/* 8192 words of 16-bit color, format: 0x0BGR (4 bits each) */
+#define VGA_RAM_SIZE 4096
+#define SCREEN_WIDTH 800
 
+/* Colors in 0BGR format */
+#define RED   0x000F
+#define GREEN 0x00F0
+#define BLUE  0x0F00
+#define WHITE 0x0FFF
+#define BLACK 0x0000
 
 int main(){
-	int x = 0000;
-	
-	volatile uint32_t *ram = &SDRAM;
+	volatile uint32_t *vga = &SDRAM;
+	int x, y;
 
-	for(x=0; x<VGA_RAM_SIZE; x++){
-		ram[x]=0x000F; // 0BGR
-		delay_(10000);
-	}	
-	
-	
+	/* Clear VGA RAM to black */
+	for (x = 0; x < VGA_RAM_SIZE; x++) {
+		vga[x] = BLACK;
+	}
+
+	/* Draw a 64x64 red square starting at pixel (0,0).
+	 * With 800px width, row y starts at address y*800.
+	 * We can only address up to 4095 (daddress limit),
+	 * so rows 0..4 are fully addressable (5 * 800 = 4000).
+	 * Draw a 64-pixel wide stripe for 5 rows as test. */
+	for (y = 0; y < 5; y++) {
+		for (x = 0; x < 64; x++) {
+			int addr = y * SCREEN_WIDTH + x;
+			if (addr < VGA_RAM_SIZE) {
+				vga[addr] = RED;
+			}
+		}
+	}
+
+	/* Draw a green stripe next to it */
+	for (y = 0; y < 5; y++) {
+		for (x = 64; x < 128; x++) {
+			int addr = y * SCREEN_WIDTH + x;
+			if (addr < VGA_RAM_SIZE) {
+				vga[addr] = GREEN;
+			}
+		}
+	}
+
+	/* Draw a blue stripe next to that */
+	for (y = 0; y < 5; y++) {
+		for (x = 128; x < 192; x++) {
+			int addr = y * SCREEN_WIDTH + x;
+			if (addr < VGA_RAM_SIZE) {
+				vga[addr] = BLUE;
+			}
+		}
+	}
+
+	/* Infinite loop */
+	while(1);
 
 	return 0;
 }
