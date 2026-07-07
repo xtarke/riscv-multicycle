55d655ff321bc45861111e9701cf3bc92f388239
SofiaLee07 <sofia.l@aluno.ifsc.edu.br>
2026-07-07 18:49:20 -0300

    adicionando .c e .h

---
+++ software/SQRT/raiz.h
@@ -0,0 +1,25 @@
+#ifndef __RAIZ_H
+#define __RAIZ_H
+
+#include <stdint.h>
+#include "../_core/hardware.h"
+
+/*-------------------------------------------------------
+ * Registradores do periférico RAIZ
+ *
+ * Offset 0 -> Operando (SW)
+ * Offset 1 -> Resultado
+ * Offset 2 -> Resto
+ *------------------------------------------------------*/
+
+#define SQRT_INPUT      (*(&SQRT_BASE_ADDRESS))
+#define SQRT_RESULT     (*(&SQRT_BASE_ADDRESS + 1))
+#define SQRT_REMAINDER  (*(&SQRT_BASE_ADDRESS + 2))
+
+uint16_t raiz_get_input(void);
+
+uint8_t raiz_get_result(void);
+
+uint8_t raiz_get_remainder(void);
+
+#endif
