28b316b5c1e795f563f20be3f3a7a401ee299089
alejopm03 <perdomomilar@gmail.com>
2026-07-12 21:48:49 -0300

    SDRAM: Create cache state machina bare bones

--- peripherals/sdram/sdram_cache.vhd
+++ peripherals/sdram/sdram_cache.vhd
@@ -34,10 +34,18 @@ end entity sdram_cache;
 
 architecture sdram_cache_behavior of sdram_cache is
 
-  type cache_state_t is (INIT, IDLE, READING, WRITING, CLEARING);
+  type cache_state_t is (
+    CACHE_STATE_INIT,
+    CACHE_STATE_IDLE,
+    CACHE_STATE_READING,
+    CACHE_STATE_WRITING,
+    CACHE_STATE_RX_CACHE_MISS
+  );
 
   constant READ_FIFO_SIZE : natural := 512;
   constant WRITE_FIFO_SIZE : natural := 16;
+  
+  constant SDRAM_READ_BURST_SIZE : natural := 8;
 
   -- Write post FIFO (fifo_16): 48-bit {addr(31:0), data(15:0)}, depth 16
   signal write_fifo_data_in  : std_logic_vector(47 downto 0);
@@ -58,6 +66,12 @@ architecture sdram_cache_behavior of sdram_cache is
   signal read_fifo_full      : std_logic;
   signal read_fifo_almost_empty : std_logic;
   signal read_fifo_used      : std_logic_vector(8 downto 0);
+  signal read_fifo_reset     : std_logic;
+
+  -- Address of the sdram that the head points --
+  signal read_fifo_head_address : std_logic_vector(32 downto 0);
+  
+  signal rx_cache_miss       : std_logic;
 
   signal cache_state         : cache_state_t
 
@@ -84,7 +98,7 @@ begin
   read_fifo : entity work.fifo_512
     port map (
       clock        => clk,
-      sclr         => reset,
+      sclr         => read_fifo_reset,
       data         => read_fifo_data_in,
       wrreq        => read_fifo_push,
       rdreq        => read_fifo_pop,
@@ -98,11 +112,38 @@ begin
   process (clk, reset)
   begin
     if (reset) then
+      read_fifo_reset <= '1';
     elsif rising_edge(clk) then
+      read_fifo_reset <= '0';
       case cache_state is
-        when IDLE =>
-          if read_fifo_used < 
-        
+        when CACHE_STATE_INIT =>
+          cache_state <= CACHE_STATE_IDLE;
+        when CACHE_STATE_IDLE =>
+          -- If idle and nothing to do fill the read fifo cache --
+          if read_fifo_used < (READ_FIFO_SIZE - SDRAM_READ_BURST_SIZE) then
+            cache_state <= CACHE_STATE_READING;
+          end if;
+          
+          -- Only start writing when read fifo has enough data --
+          if write_fifo_used /= 0 and read_fifo_almost_empty /= 0 then
+            cache_state <= CACHE_STATE_WRITING;
+          end if;
+        when CACHE_STATE_READING =>
+          if sdram_wait_request = '0' then
+            cache_state <= CACHE_STATE_IDLE;
+          end if;
+        when CACHE_STATE_WRITING =>
+          if sdram_wait_request = '0' then
+            cache_state <= CACHE_STATE_IDLE;
+          end if;
+        when CACHE_STATE_RX_CACHE_MISS => 
+          -- Reset rx fifo for one cycle and start populating --
+          if (usedw = 0) then
+            cache_state <= CACHE_STATE_IDLE;
+          end if;
+      end case;
+    end if;
+
 
 
   end process;
